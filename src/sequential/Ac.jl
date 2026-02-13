1cef071e753086ab64f08c6cd339f0d5c14a151e
zchenytt <zchenytt@gmail.com>
2026-02-13 21:55:50 +0800

    Create Ac.jl

---
+++ src/sequential/Ac.jl
@@ -0,0 +1,32 @@
+module Ac
+import ..In: T, J1, J2, O
+import JuMP
+
+build(inn) = for (j, h)=zip((J1, J2), ((0,1), (1,))), j=j
+    add(inn[j], h)
+end
+function add(m, h)
+    AC = Dict(h => get() for h=h)
+    JuMP.@variable(m, 0 <= pAC[h=h, t=1:T] <= AC[h].P_AC)
+    JuMP.@variable(m, AC[h].OH - AC[h].OΔ <= o[h=h, t=1:T] <= AC[h].OH)
+    JuMP.@variable(m, 0 <= q[h=h, t=1:T] <= 0)
+    JuMP.@constraint(m, [h=h, t=1:T],
+        (O[t]-o[h,t])AC[h].CND + AC[h].Q_I[t] -q[h,t] -pAC[h,t]AC[h].COP ==
+        (o[h,ifelse(t<T,t+1,1)]-o[h,t])AC[h].INR
+    )
+    nothing
+end
+_P_AC(OH, CND, Q_I, COP) = ceil(Int, ((maximum(O) - OH)CND + maximum(Q_I)) / COP)
+function get()
+    CND   = .5rand(1:7)
+    INR   = rand(6:20)  
+    COP   = rand(2:.5:4)
+    Q_I   = rand(3:9, T)
+    # Q_BUS = rand(25:35) 
+    OH    = rand(24:29) 
+    OΔ    = rand(4:9)   
+    P_AC  = _P_AC(OH, CND, Q_I, COP)
+    (; CND, INR, COP, Q_I, #=Q_BUS,=# OH, OΔ, P_AC)
+end
+
+end
